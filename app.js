3283d8a31d7ad56aa4563411612242642e9156b7
SrinivasaKamathR <kamathsri1997@gmail.com>
2023-03-20 12:14:10 +0530

    Creating Signup Screen Backend Apis

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const sequalize = require("./util/database");
+// const Expense = require("./models/expense");
+const User = require("./models/user");
+
+const rootDir = require("./util/path");
+// const expenseRoutes = require("./routes/expenseRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//app.use(expenseRoutes);
+app.use(userRoutes);
+
+sequalize
+  .sync()
+  .then((result) => {
+    app.listen("3000", () => {
+      // console.log(result);
+      console.log("Server listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
